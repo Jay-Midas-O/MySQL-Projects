bebd01eb15d8d6a08a427d8cfc7503a6682e4238
Joseph Owusu Afriyie <jowusu_afriyie004@st.ug.edu.gh>
2025-03-03 22:15:42 +0000

    Update Exploratory Data Analysis (Project 2).sql

--- Exploratory Data Analysis (Project 2).sql
+++ Exploratory Data Analysis (Project 2).sql
@@ -3,44 +3,59 @@
 SELECT *
 FROM layoff_staging;
 
-SELECT MAX(total_laid_off) , MAX(percentage_laid_off)
+SELECT 
+  MAX(total_laid_off), 
+  MAX(percentage_laid_off)
 FROM layoff_staging;
 
-SELECT country, SUM(total_laid_off)
+SELECT 
+  country, 
+  SUM(total_laid_off)
 FROM layoff_staging
 GROUP BY country
 ORDER BY 2 DESC;
 
-SELECT `date`, SUM(total_laid_off)
+SELECT 
+  `date`, SUM(total_laid_off)
 FROM layoff_staging
 GROUP BY `date`
 ORDER BY 1 DESC;
 
-SELECT YEAR(`date`), SUM(total_laid_off)
+SELECT 
+  YEAR(`date`), SUM(total_laid_off)
 FROM layoff_staging
 GROUP BY YEAR(`date`)
 ORDER BY 1 DESC;
 
-SELECT MONTH(`date`), SUM(total_laid_off)
+SELECT 
+  MONTH(`date`),
+  SUM(total_laid_off)
 FROM layoff_staging
 GROUP BY MONTH(`date`)
 ORDER BY 1 DESC;
 
-SELECT stage, SUM(total_laid_off)
+SELECT 
+  stage, 
+  SUM(total_laid_off)
 FROM layoff_staging
 GROUP BY stage          
 ORDER BY 2 DESC;
 
-SELECT stage, SUM(total_laid_off)
+SELECT 
+  stage, 
+  SUM(total_laid_off)
 FROM layoff_staging
 GROUP BY stage          
 ORDER BY 2 DESC;
 
 
-SELECT SUBSTRING(`date`, 1,10) AS `MONTH` -- SUM(total_laid_off)
+SELECT 
+  SUBSTRING(`date`, 1,10) AS `MONTH` -- SUM(total_laid_off)
 FROM layoff_staging;
 
-SELECT SUBSTRING(`date`, 1,7) AS `MONTH`, SUM(total_laid_off)
+SELECT 
+  SUBSTRING(`date`, 1,7) AS `MONTH`, 
+  SUM(total_laid_off)
 FROM layoff_staging
 WHERE SUBSTRING(`date`, 1,10)
 GROUP BY `MONTH`
@@ -50,18 +65,25 @@ ORDER BY 1 ASC;
 
 WITH Rolling_Total AS
 (
-SELECT SUBSTRING(`date`, 1,7) AS `MONTH`, SUM(total_laid_off) AS Total_off
+SELECT 
+  SUBSTRING(`date`, 1,7) AS `MONTH`, 
+  SUM(total_laid_off) AS Total_off
 FROM layoff_staging
 WHERE SUBSTRING(`date`, 1,10)
 GROUP BY `MONTH`
 ORDER BY 1 ASC
 )
-SELECT `MONTH`, Total_off,
-SUM(Total_off) OVER(ORDER BY `MONTH`) AS rolling_total
+SELECT 
+  `MONTH`, 
+  Total_off,
+  SUM(Total_off) OVER(ORDER BY `MONTH`) AS rolling_total
 FROM Rolling_Total;
 
 
-SELECT company, YEAR(`date`), SUM(total_laid_off)
+SELECT 
+  company, 
+  YEAR(`date`), 
+  SUM(total_laid_off)
 FROM layoff_staging
 GROUP BY company, YEAR(`date`)
 ORDER BY company DESC;
@@ -71,7 +93,10 @@ ORDER BY company DESC;
 
 WITH Company_Year (COMPANY, YEARS, TLO) AS
 (
-SELECT company, YEAR(`date`), SUM(total_laid_off)
+SELECT 
+  company, 
+  YEAR(`date`), 
+  SUM(total_laid_off)
 FROM layoff_staging
 GROUP BY company, YEAR(`date`)
 ),
